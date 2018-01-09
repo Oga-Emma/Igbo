d6d6474bcf186a27ded0e7b7ed543b4b80903030
Oga-Emma <sevenapps2018@gmail.com>
2018-01-09 00:46:43 +0000

    added_igbo_calendar_live_data

---
+++ app/src/main/java/com/example/seven/igbocalender/DateData.java
@@ -0,0 +1,74 @@
+package com.example.seven.igbocalender;
+
+/**
+ * Created by seven on 1/8/18.
+ */
+
+public class DateData {
+    String english_month;
+    String english_week;
+    String english_day;
+    String trad_month;
+    String trad_week;
+    String trad_day;
+    String onwa_izu_asaa;
+
+    public DateData() {
+    }
+
+    public String getEnglish_month() {
+        return english_month;
+    }
+
+    public void setEnglish_month(String english_month) {
+        this.english_month = english_month;
+    }
+
+    public String getEnglish_week() {
+        return english_week;
+    }
+
+    public void setEnglish_week(String english_week) {
+        this.english_week = english_week;
+    }
+
+    public String getEnglish_day() {
+        return english_day;
+    }
+
+    public void setEnglish_day(String english_day) {
+        this.english_day = english_day;
+    }
+
+    public String getTrad_month() {
+        return trad_month;
+    }
+
+    public void setTrad_month(String trad_month) {
+        this.trad_month = trad_month;
+    }
+
+    public String getTrad_week() {
+        return trad_week;
+    }
+
+    public void setTrad_week(String trad_week) {
+        this.trad_week = trad_week;
+    }
+
+    public String getTrad_day() {
+        return trad_day;
+    }
+
+    public void setTrad_day(String trad_day) {
+        this.trad_day = trad_day;
+    }
+
+    public String getOnwa_izu_asaa() {
+        return onwa_izu_asaa;
+    }
+
+    public void setOnwa_izu_asaa(String onwa_izu_asaa) {
+        this.onwa_izu_asaa = onwa_izu_asaa;
+    }
+}
